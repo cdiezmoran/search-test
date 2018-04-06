a4eab1c55ebcbca0716ace2444a6e8a62f0f18c7
cdiezmoran <cdiezmoran@gmail.com>
2018-04-06 04:28:49 -0500

    Display detailed info on comparison card

--- src/views/HomePage/ComparisonCard.js
+++ src/views/HomePage/ComparisonCard.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import uuid from 'uuid/v4';
+import { gameShape } from '@shapes';
 import Card from '@components/Card';
 
 import styles from './ComparisonCard.scss';
 
 
-const ComparisonCard = () => (
-  <Card title="Comparison">
-    <div className={styles.Column}>
-      <p>Data Item 1</p>
-    </div>
-    <div className={styles.Column}>
-      <p>Data Item 1</p>
-    </div>
-  </Card>
-);
+const ComparisonCard = ({ leftResult, rightResult }) => {
+  const renderRows = () =>
+    Object.keys(leftResult.detailedFields).map(key => (
+      <div className={styles.Row} key={uuid()}>
+        <p>{leftResult ? leftResult.detailedFields[key] : ''}</p>
+        <p>{rightResult ? rightResult.detailedFields[key] : ''}</p>
+      </div>
+    ));
+
+  const renderContent = () => {
+    if (leftResult || rightResult) {
+      return (
+        <div className={styles.List}>
+          {renderRows()}
+        </div>
+      );
+    }
+
+    return <p>Search for games to start comparing.</p>;
+  };
+
+  return (
+    <Card title="Comparison">
+      {renderContent()}
+    </Card>
+  );
+};
+
+ComparisonCard.propTypes = {
+  leftResult: gameShape,
+  rightResult: gameShape,
+};
+
+ComparisonCard.defaultProps = {
+  leftResult: null,
+  rightResult: null
+};
 
 export default ComparisonCard;
