1a3be98a0fea165d17574a8edd4ad02c2d17625d
cdiezmoran <cdiezmoran@gmail.com>
2018-04-06 00:56:48 -0500

    Connect redux state to home page

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { fetchGameById } from '@actions/games';
 
 import Home from '@views/HomePage/Home';
+
+const mapStateToProps = ({ games: { items, hasError, isLoading } }) => (
+  {
+    games: items,
+    hasError,
+    isLoading
+  }
+);
 
 const mapDispatchToProps = dispatch => ({
   fetchGame: id => dispatch(fetchGameById(id))
@@ -10,4 +19,4 @@ const mapDispatchToProps = dispatch => ({
 
 const HomePage = () => <Home />;
 
-export default HomePage;
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
